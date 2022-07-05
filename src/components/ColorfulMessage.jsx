2b574ad0b23ca8be29e9c7673cc7676ab6865b14
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-05 10:31:34 +0900

    エクスポートについて

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColofulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
     // オブジェクトがプロパティ名と当てはめる変数名が同じときは、：以降の変数名がなくてもOK
@@ -9,5 +9,3 @@ const ColofulMessage = (props) => {
   };
   return <p style={contentStyle}>{children}</p>;
 };
-
-export default ColofulMessage;
